ae28756b93c449b43efdf662e3387b6ab1051dc1
Mohammadabutaimeh <hamota741994@gmail.com>
2023-12-22 06:02:35 +0300

    adding the assertion to the code

--- src/AlmosaferWebsite/AlmosaferTestCases.java
+++ src/AlmosaferWebsite/AlmosaferTestCases.java
@@ -14,77 +14,155 @@ public class AlmosaferTestCases extends Parameters {
 
 	}
 
-	@Test()
-	public void FlightBookingTest() {
-// 1- go to this website and let the language of the website to be randomly change arabic or english
+	@Test(priority = 1)
+	public void GoToWibsite() throws InterruptedException {
+		
 		driver.get(WebsiteUrl);
-
 		WebElement PopupButton = driver.findElement(By.className(PopupClassName));
 		PopupButton.click();
-//	2- go to flight tab	
+		Thread.sleep(2000);
+		
+		String ActualURL = driver.findElement(By.tagName("link")).getAttribute("href");
+		String ExpectedURL = WebsiteUrl;
+		myAssert.assertEquals(ActualURL, ExpectedURL, "URLs do not match");
+		myAssert.assertAll();
+
+	}
+
+	@Test(priority = 2)
+	public void GoToFlightTab() {
+		
 		WebElement FlightTab = driver.findElement(By.id(FlightTabId));
 		FlightTab.click();
+		
+		String ActualActiveTab = FlightTab.getAttribute("class");
+		String ExpectedActiveTab = "active";
+		myAssert.assertTrue(ActualActiveTab.contains(ExpectedActiveTab), "FlightTab not the active tab");
+		myAssert.assertAll();
+	}
 
+	@Test(priority = 3)
+	public void FillCities() throws InterruptedException {
 		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
-
+		WebElement RandomClick = driver.findElement(By.cssSelector(RandomClickCssSelector));
 		if (ActualLanguage.contains("ar")) {
-//	3- fill any random city in the from input and any random city in the to city
 			WebElement FromInput = driver.findElement(By.className(FromInputClassName));
 			FromInput.sendKeys(ARFromCity);
-			WebElement RandomClick = driver.findElement(By.cssSelector(RandomClickCssSelector));
+			Thread.sleep(1000);
 			RandomClick.click();
 			WebElement ToInput = driver.findElement(By.cssSelector(ARToInputCssSelector));
 			ToInput.sendKeys(ARToCity);
+			Thread.sleep(1000);
 			RandomClick.click();
-//	4- change the depature date to be at the begining of february and the return date to be any date at the end of february
+			Thread.sleep(2000);
+		} else {
+			WebElement FromInput = driver.findElement(By.className(FromInputClassName));
+			FromInput.sendKeys(ENFromCity);
+			Thread.sleep(1000);
+			RandomClick.click();
+			WebElement ToInput = driver.findElement(By.cssSelector(ENToInputCssSelector));
+			ToInput.sendKeys(ENToCity);
+			Thread.sleep(1000);
+			RandomClick.click();
+			Thread.sleep(2000);
+		}
+
+	}
+
+	@Test(priority = 4)
+	public void ChooseDates() throws InterruptedException {
+
+		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+		WebElement RandomClick = driver.findElement(By.cssSelector(RandomClickCssSelector));
+		if (ActualLanguage.contains("ar")) {
 			WebElement FromDate = driver.findElement(By.cssSelector(ARFromDateCssSelector));
 			FromDate.click();
 			WebElement NextMonthButton = driver.findElement(By.cssSelector(ARNextMonthButtonCssSelector));
 			NextMonthButton.click();
+			Thread.sleep(1000);
 			WebElement FromDay = driver.findElement(By.cssSelector(FromDayCssSelector));
 			FromDay.click();
 			RandomClick.click();
+			Thread.sleep(1000);
 			WebElement ToDate = driver.findElement(By.cssSelector(ARToDateCssSelector));
 			ToDate.click();
+			Thread.sleep(1000);
 			WebElement ToDay = driver.findElement(By.cssSelector(ToDayCssSelector));
 			ToDay.click();
-//	5- change the class to be business man class then hit search button
-			WebElement FlightClass = driver.findElement(By.cssSelector(ClassCssSelector));
-			FlightClass.click();
-			WebElement BusinessClass = driver.findElement(By.cssSelector(ClassBusinessCssSelector));
-			BusinessClass.click();
-			WebElement SearchButton = driver.findElement(By.cssSelector(SearchButtonCssSelector));
-			SearchButton.click();
-
+			Thread.sleep(2000);
+			
 		} else {
-//	3- fill any random city in the from input and any random city in the to city
-			WebElement FromInput = driver.findElement(By.className(FromInputClassName));
-			FromInput.sendKeys(ENFromCity);
-			WebElement RandomClick = driver.findElement(By.cssSelector(RandomClickCssSelector));
-			RandomClick.click();
-			WebElement ToInput = driver.findElement(By.cssSelector(ENToInputCssSelector));
-			ToInput.sendKeys(ENToCity);
-			RandomClick.click();
-//	4- change the depature date to be at the begining of february and the return date to be any date at the end of february
 			WebElement FromDate = driver.findElement(By.cssSelector(ENFromDateCssSelector));
 			FromDate.click();
 			WebElement NextMonthButton = driver.findElement(By.cssSelector(ENNextMonthButtonCssSelector));
 			NextMonthButton.click();
+			Thread.sleep(1000);
 			WebElement FromDay = driver.findElement(By.cssSelector(FromDayCssSelector));
 			FromDay.click();
 			RandomClick.click();
+			Thread.sleep(1000);
 			WebElement ToDate = driver.findElement(By.cssSelector(ENToDateCssSelector));
 			ToDate.click();
+			Thread.sleep(1000);
 			WebElement ToDay = driver.findElement(By.cssSelector(ToDayCssSelector));
 			ToDay.click();
-//	5- change the class to be business man class then hit search button
-			WebElement FlightClass = driver.findElement(By.cssSelector(ClassCssSelector));
-			FlightClass.click();
-			WebElement BusinessClass = driver.findElement(By.cssSelector(ClassBusinessCssSelector));
-			BusinessClass.click();
+			Thread.sleep(2000);
+		}
+		
+		String ActualFromDate =  driver.findElement(By.cssSelector(ActualFromDateCssSelector)).getText();
+		String ActualToDate =  driver.findElement(By.cssSelector(ActualToDateCssSelector)).getText();
+		String ExpectedFromDate = "5";
+		String ExpectedToDate = "27";
+		myAssert.assertTrue(ActualFromDate.contains(ExpectedFromDate), "FromDate do not match");
+		myAssert.assertTrue(ActualToDate.contains(ExpectedToDate), "ToDate do not match");
+		myAssert.assertAll();
+		
+	}
+
+	@Test(priority = 5)
+	public void ChooseBusinessClass() throws InterruptedException {
+		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+		WebElement FlightClass = driver.findElement(By.cssSelector(ClassCssSelector));
+		FlightClass.click();
+		Thread.sleep(1000);
+		WebElement BusinessClass = driver.findElement(By.cssSelector(ClassBusinessCssSelector));
+		BusinessClass.click();
+		Thread.sleep(2000);
+		
+		if (ActualLanguage.contains("ar")) {
+		String ActualFlightClass = driver.findElement(By.cssSelector(ActualFlightClassCssSelector)).getText();
+		String ExpectedFlightClass = "رجال الأعمال";
+		myAssert.assertTrue(ActualFlightClass.contains(ExpectedFlightClass), "Business Class not chosen");
+		myAssert.assertAll();
+		
+		WebElement SearchButton = driver.findElement(By.cssSelector(SearchButtonCssSelector));
+		SearchButton.click();
+		Thread.sleep(5000);
+		
+		String ActualResultPage = driver.findElement(By.cssSelector(".sc-dpiBDp.ciGvla")).getText();
+		String ExpectedResultPage = "العثور";
+		myAssert.assertTrue(ActualResultPage.contains(ExpectedResultPage), "transfer to result page not secessful");
+		myAssert.assertAll();
+		System.out.println(ActualResultPage);
+	
+		}else {
+			String ActualFlightClass = driver.findElement(By.cssSelector(ActualFlightClassCssSelector)).getText();
+			String ExpectedFlightClass = "Business";
+			myAssert.assertTrue(ActualFlightClass.contains(ExpectedFlightClass), "Business Class not chosen");
+			myAssert.assertAll();
+			
 			WebElement SearchButton = driver.findElement(By.cssSelector(SearchButtonCssSelector));
 			SearchButton.click();
+			Thread.sleep(5000);
+			
+			String ActualResultPage = driver.findElement(By.cssSelector(ActualResultPageCssSelector)).getText();
+			String ExpectedResultPage = "found";
+			myAssert.assertTrue(ActualResultPage.contains(ExpectedResultPage), "transfer to result page not secessful");
+			myAssert.assertAll();
+			System.out.println(ActualResultPage);
+
 		}
+		
 	}
 
 	@AfterTest
--- src/AlmosaferWebsite/Parameters.java
+++ src/AlmosaferWebsite/Parameters.java
@@ -2,20 +2,26 @@ package AlmosaferWebsite;
 
 import java.util.Random;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
 
 public class Parameters {
 
 	static WebDriver driver = new ChromeDriver();
 	static Random rand = new Random();
+    static SoftAssert myAssert = new SoftAssert();
+
 
 	static String[] URLS = { "https://www.almosafer.com/ar", "https://www.almosafer.com/en" };
 	static int RandomlndexForTheUrls = rand.nextInt(2);
 	static String WebsiteUrl = URLS[RandomlndexForTheUrls];
 	static String PopupClassName = "cta__image";
-
 	static String FlightTabId = "uncontrolled-tab-example-tab-flights";
+	
+	
+
 
 	static String[] ENFromCities = { "Cairo", "Tokyo", "Berlin", "Dubai", "Chicago" };
 	static String[] ARFromCities = { "القاهرة", "طوكيو", "برلين", "دبي", "شيكاغو" };
@@ -33,7 +39,7 @@ public class Parameters {
 	static String ARToInputCssSelector = "input[placeholder='محطة الوصول']";
 	static String ENToInputCssSelector = "input[placeholder='Destination']";
 	static String RandomClickCssSelector = "img[alt='hero-image']";
-
+	
 	static String ARFromDateCssSelector = ".sc-OxbzP.sc-lnrBVv.fIoFkY";
 	static String ENFromDateCssSelector = ".sc-OxbzP.sc-lnrBVv.gKbptE";
 	static String ARNextMonthButtonCssSelector = ".sc-eHgmQL.kgqFKY";
@@ -42,10 +48,14 @@ public class Parameters {
 	static String ARToDateCssSelector = ".sc-OxbzP.sc-bYnzgO.iPcZol";
 	static String ENToDateCssSelector = ".sc-OxbzP.sc-bYnzgO.bojUIa";
 	static String ToDayCssSelector = "div[aria-label='Tue Feb 27 2024']";
+	
+	static String ActualFromDateCssSelector = ".sc-OxbzP.sc-lnrBVv.fIoFkY";
+	static String ActualToDateCssSelector = ".sc-OxbzP.sc-bYnzgO.iPcZol";
 
 	static String ClassCssSelector = ".sc-dPPMrM.fOQKsp";
 	static String ClassBusinessCssSelector = "div[data-testid='FlightSearchCabinSelection__BusinessOption']";
+	static String ActualFlightClassCssSelector = ".sc-dPPMrM.fOQKsp";
 
 	static String SearchButtonCssSelector = "div[class='sc-hARARD jGOsnk d-none d-md-block'] button[type='button']";
-
+	static String ActualResultPageCssSelector = ".sc-dPPMrM.fOQKsp";
 }
